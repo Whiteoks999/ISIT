2946581fac218a052d43d1f74aaf5a5ef29bf701
Oksana <semixina2003@mail.ru>
2023-12-04 22:46:03 +0400

    commit1

---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Обфускатор
+{
+    public partial class Form1 : Form
+    {
+        Obfuskator obfuskator;
+        public Form1()
+        {
+            InitializeComponent();
+            obfuskator=Obfuskator.GetObfuskator();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(); //открытие диалогового окна
+            openFileDialog1.Filter = "Text|*.txt"; //расширение файла
+
+            string readFile = File.ReadAllText(openFileDialog1.FileName); //считывание файла
+            obfuskator.addSourceCode(readFile); //передача данных файла в обфускатор
+
+            richTextBox1.Text = obfuskator.obfuscate(); //обфусцирование программного кода и его вывод в тектовое поле
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text|*.txt";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) {
+                richTextBox1.SaveFile(saveFileDialog1.FileName);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Obfuskator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Обфускатор
+{
+    internal class Obfuskator
+    {
+        public static Obfuskator obfuskator = new Obfuskator();
+        private string sourceCode;
+        private string obfuscatedСode;
+        private Obfuskator() { }
+        //Какие-то изменения
+
+        public static Obfuskator GetObfuskator() { return obfuskator; }
+        //Какие-то изменения
+        public void addSourceCode(string sourceCode) { 
+        this.sourceCode = sourceCode;
+        }
+
+        public string obfuscate() {
+            string st = sourceCode.Replace("\r\n", " "); //удаление переноса строк
+            string[] wordsCode = st.Split(' '); 
+            string oldClassName = "";
+            string newClassName = randomNameClass();
+            bool flag = false;
+
+            obfuscatedСode += "/*" + randomComment() + "*/"; //добавление избыточных комментариев
+
+            foreach (string word in wordsCode) { //поиск имени класса
+                if (flag) oldClassName = word;
+                flag = false;
+                if (word.Equals("class")) flag=true;
+            }
+            
+            foreach (string word in wordsCode) //замена старого имени класса на новое
+            {
+                if (word.Equals(oldClassName)) {
+                    obfuscatedСode += " "+newClassName;
+                }
+                else if(word.Equals(oldClassName+"();")) obfuscatedСode += " " + newClassName+"();";
+
+                else
+                obfuscatedСode += " "+word;
+            }
+
+            obfuscatedСode += "/*" + randomComment() + "*/"; //добавление избыточных комментариев
+
+            return obfuscatedСode;
+        }
+
+        private string randomNameClass() {
+            char[] letters = "ABCDEuGeHIrJfvKzLbfMbNmkOPQRSTUVWXYZ".ToCharArray();
+
+            Random rand = new Random();
+                string word = "";
+                for (int i = 1; i <= 30; i++)
+                {
+                    int letter_num = rand.Next(0, letters.Length - 1);
+                    word += letters[letter_num];
+                }  
+           return word;
+        }
+
+        private string randomComment()
+        {
+            char[] letters = "qwertyuiopasdfghjklzxcvnm".ToCharArray();
+            Random rand = new Random();
+            string word = "";
+            string comment = "";
+            for (int i = 1; i <= 30; i++) {
+                word = "";
+                for (int j = 1; j <= 5; j++)
+            {
+                int letter_num = rand.Next(0, letters.Length - 1);
+                word += letters[letter_num];
+            }
+                comment += " " + word;
+            }
+            return comment;
+        }
+
+
+    }
+}
